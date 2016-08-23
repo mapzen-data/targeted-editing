5a09e59451ed92c0dae32a98957740d9cc7c34e1
Indy Hurt <indy.hurt@gmail.com>
2016-08-23 11:18:41 -0700

    update library and museum queries

--- queries/collections_sql.sql
+++ queries/collections_sql.sql
@@ -10,20 +10,21 @@ insert into collections (query_name, value) values ('art_gallery_points', (selec
 insert into collections (query_name, value) values ('art_gallery_polygons', (select count(*) from planet_osm_polygon where (tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ?| array ['museum','gallery'])) or building in ('gallery', 'art_gallery') and (osm_id > 0)));
 
 
--- QUERIES FOR LIBRARIES (add museum=gallery)
+-- QUERIES FOR LIBRARIES
 
 -- How many library points are there?
-insert into collections (query_name, value) values ('library_points', (select count(*) from planet_osm_point where (amenity = 'library' or building = 'library' or tags? 'library') and (osm_id > 0)));
+insert into collections (query_name, value) values ('library_points', (select count(*) from planet_osm_point where (amenity = 'library' or building = 'library' or tags? 'library' or tags @> '"museum"=>"gallery"'::hstore) and (osm_id > 0)));
 
 -- How many library polygons are there?
-insert into collections (query_name, value) values ('library_polygons', (select count(*) from planet_osm_polygon where (amenity = 'library' or building = 'library' or tags? 'library') and (osm_id > 0)));
+insert into collections (query_name, value) values ('library_polygons', (select count(*) from planet_osm_polygon where (amenity = 'library' or building = 'library' or tags? 'library' or tags @> '"museum"=>"gallery"'::hstore) and (osm_id > 0)));
 
--- QUERIES FOR MUSEUMS (add museum=* w/o gallery)
+-- QUERIES FOR MUSEUMS 
 
 -- How many museum points are there?
-insert into collections (query_name, value) values ('museum_points', (select count(*) from planet_osm_point where (tourism = 'museum' or amenity = 'museum' or building = 'museum' or tags? 'museum') and (osm_id > 0)));
+insert into collections (query_name, value) values ('museum_points', (select count(*) from planet_osm_point where (tourism = 'museum' or amenity = 'museum' or building = 'museum') or (tags? 'museum' and tags -> 'museum' != 'gallery') and (osm_id > 0)));
 
 -- How many museum polygons are there?
-insert into collections (query_name, value) values ('museum_polygons', (select count(*) from planet_osm_polygon where (tourism = 'museum' or amenity = 'museum' or building = 'museum' or tags? 'museum') and (osm_id > 0)));
+insert into collections (query_name, value) values ('museum_polygons', (select count(*) from planet_osm_polygon where (tourism = 'museum' or amenity = 'museum' or building = 'museum') or (tags? 'museum' and tags -> 'museum' != 'gallery') and (osm_id > 0)));
 
 select * from collections;
+
