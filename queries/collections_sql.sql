bd481366d07da4c4f43f88b673c87d1dccba1ed3
Indy Hurt <indy.hurt@gmail.com>
2016-08-24 16:03:02 -0700

    update collections sql
    add attribute queries for the libraries and museums

--- queries/collections_sql.sql
+++ queries/collections_sql.sql
@@ -51,19 +51,96 @@ insert into collections (query_name, value) values ('art_gallery_polygons_with_f
 
 -- QUERIES FOR LIBRARIES
 
+
+  -- library points
 -- How many library points are there?
 insert into collections (query_name, value) values ('library_points', (select count(*) from planet_osm_point where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0))));
 
+-- How many library points have addresses?
+insert into collections (query_name, value) values ('library_points_with_housenumber', (select count(*) from planet_osm_point where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and "addr:housenumber" is not null))));
+
+-- How many library points have websites?
+insert into collections (query_name, value) values ('library_points_with_website', (select count(*) from planet_osm_point where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and (tags ?| array ['website','url'])))));
+
+-- How many library points have phone numbers?
+insert into collections (query_name, value) values ('library_points_with_phone', (select count(*) from planet_osm_point where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and (tags ? 'phone')))));
+
+-- How many library points have opening hours?
+insert into collections (query_name, value) values ('library_points_with_hours', (select count(*) from planet_osm_point where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and (tags ? 'opening_hours' or tags ? 'operating_hours')))));
+
+-- How many library points have wheelchair accessibility information?
+insert into collections (query_name, value) values ('library_points_with_wheelchair', (select count(*) from planet_osm_point where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and (tags ? 'wheelchair')))));
+
+-- How many library points have fee information?
+insert into collections (query_name, value) values ('library_points_with_fee', (select count(*) from planet_osm_point where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and (tags ? 'fee')))));
+
+  -- library polygons
 -- How many library polygons are there?
 insert into collections (query_name, value) values ('library_polygons', (select count(*) from planet_osm_polygon where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0))));
 
+-- How many library polygons have addresses?
+insert into collections (query_name, value) values ('library_polygons_with_housenumber', (select count(*) from planet_osm_polygon where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and "addr:housenumber" is not null))));
+
+-- How many library polygons have websites?
+insert into collections (query_name, value) values ('library_polygons_with_website', (select count(*) from planet_osm_polygon where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and (tags ?| array ['website','url'])))));
+
+-- How many library polygons have phone numbers?
+insert into collections (query_name, value) values ('library_polygons_with_phone', (select count(*) from planet_osm_polygon where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and (tags ? 'phone')))));
+
+-- How many library polygons have opening hours?
+insert into collections (query_name, value) values ('library_polygons_with_hours', (select count(*) from planet_osm_polygon where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and (tags ? 'opening_hours' or tags ? 'operating_hours')))));
+
+-- How many library polygons have wheelchair accessibility information?
+insert into collections (query_name, value) values ('library_polygons_with_wheelchair', (select count(*) from planet_osm_polygon where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and (tags ? 'wheelchair')))));
+
+-- How many library polygons have fee information?
+insert into collections (query_name, value) values ('library_polygons_with_fee', (select count(*) from planet_osm_polygon where ((amenity = 'library' or building = 'library' or tags ? 'library') and (osm_id > 0 and (tags ? 'fee')))));
+
 -- QUERIES FOR MUSEUMS 
 
+  -- museum points
 -- How many museum points are there?
 insert into collections (query_name, value) values ('museum_points', (select count(*) from planet_osm_point where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0))));
 
+-- How many museum points have addresses?
+insert into collections (query_name, value) values ('museum_points_with_housenumber', (select count(*) from planet_osm_point where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and "addr:housenumber" is not null))));
+
+-- How many museum points have websites?
+insert into collections (query_name, value) values ('museum_points_with_website', (select count(*) from planet_osm_point where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and (tags ?| array ['website','url'])))));
+
+-- How many museum points have phone numbers?
+insert into collections (query_name, value) values ('museum_points_with_phone', (select count(*) from planet_osm_point where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and (tags ? 'phone')))));
+
+-- How many museum points have opening hours?
+insert into collections (query_name, value) values ('museum_points_with_hours', (select count(*) from planet_osm_point where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and (tags ? 'opening_hours' or tags ? 'operating_hours')))));
+
+-- How many museum points have wheelchair accessibility information?
+insert into collections (query_name, value) values ('museum_points_with_wheelchair', (select count(*) from planet_osm_point where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and (tags ? 'wheelchair')))));
+
+-- How many museum points have fee information?
+insert into collections (query_name, value) values ('museum_points_with_fee', (select count(*) from planet_osm_point where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and (tags ? 'fee')))));
+
+  -- museum polygons
 -- How many museum polygons are there?
 insert into collections (query_name, value) values ('museum_polygons', (select count(*) from planet_osm_polygon where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0))));
+
+-- How many museum polygons have addresses?
+insert into collections (query_name, value) values ('museum_polygons_with_housenumber', (select count(*) from planet_osm_polygon where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and "addr:housenumber" is not null))));
+
+-- How many museum polygons have websites?
+insert into collections (query_name, value) values ('museum_polygons_with_website', (select count(*) from planet_osm_polygon where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and (tags ?| array ['website','url'])))));
+
+-- How many museum polygons have phone numbers?
+insert into collections (query_name, value) values ('museum_polygons_with_phone', (select count(*) from planet_osm_polygon where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and (tags ? 'phone')))));
+
+-- How many museum polygons have opening hours?
+insert into collections (query_name, value) values ('museum_polygons_with_hours', (select count(*) from planet_osm_polygon where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and (tags ? 'opening_hours' or tags ? 'operating_hours')))));
+
+-- How many museum polygons have wheelchair accessibility information?
+insert into collections (query_name, value) values ('museum_polygons_with_wheelchair', (select count(*) from planet_osm_polygon where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and (tags ? 'wheelchair')))));
+
+-- How many museum polygons have fee information?
+insert into collections (query_name, value) values ('museum_polygons_with_fee', (select count(*) from planet_osm_polygon where ((tourism = 'museum' or amenity = 'museum' or building = 'museum' or (tags ? 'museum' and tags -> 'museum' != 'gallery')) and (osm_id > 0 and (tags ? 'fee')))));
 
 select * from collections;
 
