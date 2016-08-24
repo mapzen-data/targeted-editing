93858d72f8c1c47921f167fa1d4e5af5bb00f142
Indy Hurt <indy.hurt@gmail.com>
2016-08-24 15:16:18 -0700

    update collections sql
    add gallery attributes

--- queries/collections_sql.sql
+++ queries/collections_sql.sql
@@ -3,11 +3,50 @@ create table collections (query_name text, value real);
 
 -- QUERIES FOR GALLERIES
 
+  -- gallery points
 -- How many gallery points are there?
 insert into collections (query_name, value) values ('art_gallery_points', (select count(*) from planet_osm_point where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0))));
 
+-- How many gallery points have addresses?
+insert into collections (query_name, value) values ('art_gallery_points_with_housenumber', (select count(*) from planet_osm_point where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and "addr:housenumber" is not null))));
+
+-- How many gallery points have websites?
+insert into collections (query_name, value) values ('art_gallery_points_with_website', (select count(*) from planet_osm_point where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and (tags ?| array ['website','url'])))));
+
+-- How many gallery points have phone numbers?
+insert into collections (query_name, value) values ('art_gallery_points_with_phone', (select count(*) from planet_osm_point where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and (tags ? 'phone')))));
+
+-- How many gallery points have opening hours?
+insert into collections (query_name, value) values ('art_gallery_points_with_hours', (select count(*) from planet_osm_point where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and (tags ? 'opening_hours' or tags ? 'operating_hours')))));
+
+-- How many gallery points have wheelchair accessibility information?
+insert into collections (query_name, value) values ('art_gallery_points_with_wheelchair', (select count(*) from planet_osm_point where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and (tags ? 'wheelchair')))));
+
+-- How many gallery points have fee information?
+insert into collections (query_name, value) values ('art_gallery_points_with_fee', (select count(*) from planet_osm_point where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and (tags ? 'fee')))));
+
+  -- gallery polygons
 -- How many gallery polygons are there?
 insert into collections (query_name, value) values ('art_gallery_polygons', (select count(*) from planet_osm_polygon where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0))));
+
+-- How many gallery polygons have addresses?
+insert into collections (query_name, value) values ('art_gallery_polygons_with_housenumber', (select count(*) from planet_osm_polygon where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and "addr:housenumber" is not null))));
+
+-- How many gallery polygons have websites?
+insert into collections (query_name, value) values ('art_gallery_polygons_with_website', (select count(*) from planet_osm_polygon where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and (tags ?| array ['website','url'])))));
+
+-- How many gallery polygons have phone numbers?
+insert into collections (query_name, value) values ('art_gallery_polygons_with_phone', (select count(*) from planet_osm_polygon where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and (tags ? 'phone')))));
+
+-- How many gallery polygons have opening hours?
+insert into collections (query_name, value) values ('art_gallery_polygons_with_hours', (select count(*) from planet_osm_polygon where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and (tags ? 'opening_hours' or tags ? 'operating_hours')))));
+
+-- How many gallery polygons have wheelchair accessibility information?
+insert into collections (query_name, value) values ('art_gallery_polygons_with_wheelchair', (select count(*) from planet_osm_polygon where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and (tags ? 'wheelchair')))));
+
+-- How many gallery polygons have fee information?
+insert into collections (query_name, value) values ('art_gallery_polygons_with_fee', (select count(*) from planet_osm_polygon where ((tourism in ('gallery', 'art_gallery', 'Art Gallery', 'Art_gallery') or amenity in ('gallery', 'art_gallery', 'arts_gallery', 'contemporary_art_gallery') or (tags ? 'gallery') or building in ('gallery', 'art_gallery')) and (osm_id > 0 and (tags ? 'fee')))));
+
 
 
 -- QUERIES FOR LIBRARIES
